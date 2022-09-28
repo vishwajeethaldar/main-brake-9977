93e0048ae39c27b9e05d5ea1b25d197902147673
Js Sanjay <mname8741@gmail.com>
2022-09-28 23:05:03 +0530

    day2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react"
 import {useSelector, useDispatch} from "react-redux"
+import Timer_Tracker from "./Components/Time_Tracker/Timer_Tracker";
 import { useAppDispatch } from "./hooks/user";
 import { login } from "./store/users/users.action"
 
@@ -17,17 +18,18 @@ function App() {
   const user = {
     email:"p@gmail.com", password:"12345678"
   }
-useEffect(()=>{
-  dispatch(login(user))
-})
+// useEffect(()=>{
+//   dispatch(login(user))
+// })
 
   return (
       <div>
-      <form>
+      {/* <form>
         <input type={"email"} name={"email"}/>
         <input type={"text"} name={"password"}/>
         <button type="submit">Login</button>
-      </form>
+      </form> */}
+      <Timer_Tracker/>
     </div>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,13 @@ import App from './App'
 import store from './store/store'
 import {Provider} from 'react-redux'
 import {BrowserRouter} from 'react-router-dom'
-
+import {ChakraProvider} from "@chakra-ui/react"
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <BrowserRouter>
+    <ChakraProvider>
     <Provider store={store}>
         <App />
     </Provider>
+    </ChakraProvider>
     </BrowserRouter>
 )
