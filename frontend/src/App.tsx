35741615934ad53ffd139a1a281bce63158b2be6
pratik kharat <pratik29march@gmail.com>
2022-09-29 23:29:16 +0530

    updated day3 task

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,12 @@ import { useEffect } from "react"
 import {useSelector, useDispatch} from "react-redux"
 import { useAppDispatch } from "./hooks/user";
 import { login } from "./store/users/users.action"
+import Navbar from "./Components/Navbar/NavbarHome"
+import SecondPart from "./Components/Homepage/SecondPart"
+import ThirdPart from "./Components/Homepage/ThirdPart"
+import FourthPart from "./Components/Homepage/FourthPart"
+import FifthPart from "./Components/Homepage/Fifthpart"
+import SixthPart from "./Components/Homepage/SixthPart"
 
 type stateInter = {
   loading:boolean;
@@ -23,11 +29,19 @@ useEffect(()=>{
 
   return (
       <div>
-      <form>
+        <Navbar/>
+        <SecondPart/>
+        <ThirdPart/>
+        <FourthPart/>
+        <FifthPart/>
+        <SixthPart/>
+        
+       
+      {/* <form>
         <input type={"email"} name={"email"}/>
         <input type={"text"} name={"password"}/>
         <button type="submit">Login</button>
-      </form>
+      </form> */}
     </div>
   )
 }
---
+++ frontend/src/Components/Homepage/Homepagecss.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+
+export const SECOND_PAGE_H1= styled.h1`
+font-size: 34px;
+color: #3d4853;
+margin-left:500px;
+margin-top:100px
+// font-family: sans-serif;`;
+
+
+
+export const SECOND_PAGE_H2= styled.h2`
+font-size: 20.8px;
+color: #5a6b7b;
+margin-left:400px;
+font-family: sans-serif;`;
