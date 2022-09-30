76a26b022b8709cf83c401a35f66eaf30db7cafc
pratik kharat <pratik29march@gmail.com>
2022-09-30 23:17:11 +0530

    added todays files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,7 @@ import { useEffect } from "react"
 import {useSelector, useDispatch} from "react-redux"
 import { useAppDispatch } from "./hooks/user";
 import { login } from "./store/users/users.action"
-import Navbar from "./Components/Navbar/NavbarHome"
-import SecondPart from "./Components/Homepage/SecondPart"
-import ThirdPart from "./Components/Homepage/ThirdPart"
-import FourthPart from "./Components/Homepage/FourthPart"
-import FifthPart from "./Components/Homepage/Fifthpart"
-import SixthPart from "./Components/Homepage/SixthPart"
+import {MainRoutes} from "./Routes/MainRoutes"
 
 type stateInter = {
   loading:boolean;
@@ -29,13 +24,9 @@ useEffect(()=>{
 
   return (
       <div>
-        <Navbar/>
-        <SecondPart/>
-        <ThirdPart/>
-        <FourthPart/>
-        <FifthPart/>
-        <SixthPart/>
-        
+        <MainRoutes/>
+      
+       
        
       {/* <form>
         <input type={"email"} name={"email"}/>
--- frontend/src/Routes/MainRoutes.tsx
+++ frontend/src/Routes/MainRoutes.tsx
@@ -1,17 +1,28 @@
 import { Routes, Route } from "react-router-dom"
+import Clockify from "./Clockify"
+import Features from "./Features"
+
 export const MainRoutes = () => {
   return (
     <Routes>
-        <Route path="/" element={"home"} />
+    
+      {/* <Route path="/" element={<Clockify/>} /> */}
+      <Route path="/" element={<Clockify/>} />
+      <Route path="/features" element={<Features/>} />
+     
+       
+
+        {/* <Route path="/" element={"home"} />
+         <Route path="/features" element={"features"} />
         <Route path="/login" element={"Login"} />
         <Route path="/signup" element={"signup"} />
         <Route path="/downloads" element={"downloads"} />
-        <Route path="/features" element={"features"} />
+       
         <Route path="/tracker" element={"Tracker"} />
         <Route path="/projects" element={"projects"} />
         <Route path="/projects/id" element={"individual-project"} />
         <Route path="/teams" element={"teams"} />
-        <Route path="/clients" element={"clients"} />
+        <Route path="/clients" element={"clients"} /> */}
     </Routes>
   )
 }
