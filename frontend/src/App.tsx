eefe0540888fff87a9e57e43b6472b610c3d6b9c
Vishwajeet Haldar <63746278+vishwajeethaldar@users.noreply.github.com>
2022-09-30 09:51:46 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,10 @@
 
-import {useSelector, useDispatch} from "react-redux"
-import { useAppDispatch } from "./hooks/user";
 import { MainRoutes } from "./Routes/MainRoutes";
-import { login } from "./store/users/users.action"
-import Navbar from "./Components/Navbar/NavbarHome"
-import SecondPart from "./Components/Homepage/SecondPart"
-import ThirdPart from "./Components/Homepage/ThirdPart"
-import FourthPart from "./Components/Homepage/FourthPart"
-import FifthPart from "./Components/Homepage/Fifthpart"
-import SixthPart from "./Components/Homepage/SixthPart"
 
 
 function App() {
   return (
       <div>
-        <Navbar/>
-        <SecondPart/>
-        <ThirdPart/>
-        <FourthPart/>
-        <FifthPart/>
-        <SixthPart/>
-    </div>
       <MainRoutes/>
       </div>
   )
