b2d481d23030bdad0e81c3aaed4853e97d7ed0bc
vishwajeethaldar <onlyjeet3@gmail.com>
2022-10-03 21:20:13 +0530

    updated navbar

--- frontend/src/Components/Navbar/NavbarHome.tsx
+++ frontend/src/Components/Navbar/NavbarHome.tsx
@@ -1,29 +1,37 @@
 import { Box, Flex, Image, Text, Link, Button } from '@chakra-ui/react';
-
+import {useState} from 'react'
+import {BsXLg } from "react-icons/bs"
+import {FaBars} from 'react-icons/fa'
+import { Show, Hide } from '@chakra-ui/react'
 const NavbarHome = () => {
+  const [showSmNav, setShowSmNav] = useState(false)
   return (
     <Box w={"98.6vw"} boxShadow={"md"}>
-      <Flex align={"center"} px={"70px"} py={"10px"} justify={"space-between"} >
-        <Flex  align={"center"} gap="25px">
-          <Link href='/'><Image width={["150px"]} src="https://clockify.me/assets/images/clockify-logo.svg" alt='ImageNAvbar' /></Link>
-
+      <Flex align={["flex-start","flex-start","flex-start","center"]} px={"70px"} gap ="25px" py={"10px"} justify={"flex-start"} direction={["column", "column","column","row"]}>
+        <Flex align={"center"} justify={"space-between"} w={["100%","100%","100%","10%"]}>
+        <Link href='/'>
+            <Image width={["150px"]} src="https://clockify.me/assets/images/clockify-logo.svg" alt='ImageNAvbar' />
+        </Link>
+        <Show breakpoint='(max-width: 991px)'>
+            <Box _hover={{cursor:"pointer"}}>
+              {showSmNav?
+              <BsXLg onClick={()=>setShowSmNav(!showSmNav)} fontSize={"1.2em"}/>:
+              <FaBars onClick={()=>setShowSmNav(!showSmNav)} fontSize={"1.2em"}/>
+              }
+            </Box>
+        </Show>
+        </Flex>
+        <Flex  display={[showSmNav?"flex":"none",showSmNav?"flex":"none",showSmNav?"flex":"none", "flex"]} align={"center"} direction={["column","column","column","row"]} gap="25px" width={["100%","100%","100%","35%"]}>
           <Link _hover={{textDecoration:"none", color:"#03A9F4"}} textDecoration={"none"} href='/features'>
           <Text > FEATURES</Text>
           </Link>
-
-
           <Link _hover={{textDecoration:"none", color:"#03A9F4" }}  textDecoration={"none"} href='/downloads'>
           <Text> DOWNLOADS</Text>
           </Link>
-            
-         
-
         </Flex>
 
 
-
-
-        <Flex align={"center"} gap="25px">
+        <Flex display={[showSmNav?"flex":"none",showSmNav?"flex":"none",showSmNav?"flex":"none", "flex"]} justify={["center", "center", "center", "flex-end"]} align={"center"} gap="25px" direction={["column","column","column","row"]} width={["100%","100%","100%","55%"]}>
         <Link _hover={{textDecoration:"none", color:"#03A9F4"}}  textDecoration={"none"} href='/login'>
           <Text> LOGIN</Text>
           </Link>
